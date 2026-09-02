1f23b21a5b753ddf705f452ddd1569267127d2c2
Azim-Eham <azimeham84@gmail.com>
2026-09-03 03:20:09 +0600

    fixed login and sign up page navbar

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,10 +1,14 @@
 import { Suspense } from "react";
 import LoginForm from "./login-form";
+import { PublicNavbar } from "@/components/public-navbar";
 
 export default function LoginPage() {
   return (
-    <Suspense fallback={<div className="flex min-h-[calc(100vh-100px)] w-full items-center justify-center">Loading...</div>}>
-      <LoginForm />
-    </Suspense>
+    <div className="flex flex-col min-h-screen">
+      <PublicNavbar hideLinks={true} />
+      <Suspense fallback={<div className="flex min-h-[calc(100vh-100px)] w-full items-center justify-center">Loading...</div>}>
+        <LoginForm />
+      </Suspense>
+    </div>
   );
 }
